772c225a7c618d0d7cfdfb8876145e272f685b81
Ubuntu <ubuntu@ip-172-31-42-121.ap-northeast-1.compute.internal>
2022-08-22 14:16:54 +0000

    [Feat] add route and modify package for npm install in ec2 ubuntu vm

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM node:16-alpine
 
 WORKDIR /usr/src/app
 
+COPY . .
 RUN npm install
-COPY package*.json ./
-COPY . /usr/src/app
 
 EXPOSE 3000
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res)=>{
+app.get("/backend", (req, res)=>{
   return res.status(200).send("Hello world !")
 })
 
